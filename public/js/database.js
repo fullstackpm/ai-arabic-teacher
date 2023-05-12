36d5a35d6733906c10779479d11110a104588578
Saidur Sikder <uk45654185@ukmlv6dnf6g5.global.tesco.org>
2023-05-12 22:58:33 +0100

    First commit

---
+++ public/js/database.js
@@ -0,0 +1,4 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+
