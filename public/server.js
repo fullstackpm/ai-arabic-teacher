9d0cdb15b9908078c6d41f65cc74ee1b2f23dae5
Saidur Sikder <uk45654185@ukmlv6dnf6g5.cust.communityfibre.co.uk>
2023-05-19 10:54:18 +0100

    first firebase deployment

--- public/server.js
+++ public/server.js
@@ -1,10 +1,9 @@
-
-
 const express = require('express');
 const app = express();
+const path = require('path');
 
-// Serve static files from the static folder
-app.use(express.static('static'));
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
 
 // Start the server
 const port = 3000;
